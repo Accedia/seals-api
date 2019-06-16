8a97896329739a9a496f4b911a7614c4202e59ba
nikola.kostov <nikola.kostov@accedia.com>
2019-06-16 14:57:02 +0300

    Add function to import varna data

--- src/file-export/varna.py
+++ src/file-export/varna.py
@@ -1,22 +1,13 @@
 import json
 import shutil
-import urllib
 from datetime import datetime
 
-import PyPDF2
-import boto3
-import camelot #PDF table parsing libary
 import requests
-import pandas as pd
 import re
-import numpy as np
-from pymongo import MongoClient
+from tika import parser
 
 VARNA_FILE_NAME = "mvodi-2019.pdf"
 VARNA_URL = "http://www.rzi-varna.com/health/mvodi-2019.pdf"
-
-BURGAS_FILE_NAME = "tablici_monitoring_moreMZ-2019.xls"
-BURGAS_URL = "http://www.rzi-burgas.com/Vodi/tablici_monitoring_moreMZ-2019.xls"
 
 
 def degreesToDecimal(lat, lon):
@@ -31,10 +22,6 @@ def degreesToDecimal(lat, lon):
 
     return round(latitude, 6), round(longitude, 6)
 
-
-def is_nan(x):
-    return (x is np.nan or x != x)
-
 def download(url, file_name):
     r = requests.get(url, stream=True)
     if r.status_code == 200:
@@ -42,164 +29,58 @@ def download(url, file_name):
         with open(file_name, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
 
+def processFile(file_name):
+    raw = parser.from_file(file_name)
+    lineItems = raw['content'].split('\n')
+    results = []
+    tableResults = []
 
-def extractPdfFile(file_name, pages):
-    tables = camelot.read_pdf(file_name, pages=pages)
-    res = []
-    for t in tables:
-        res.append(t.df.values)
-    return res
+    for i in range(0, len(lineItems) - 1):
+        if lineItems[i] is '':
+            continue
+        elif re.match("^\d{1,2}.\d{2}.\d{4} г.", lineItems[i]):
+            measurement = lineItems[i].split(' ')
+            parsedDate = None
+            if type(measurement[2]) is datetime:
+                parsedDate = measurement[2]
+            else:
+                parsedDate = datetime.strptime(measurement[2], "%d.%m.%Y")
+            result = {
+                'measurement_date': parsedDate.isoformat(),
+                'intestinal_enterococci': measurement[5] if measurement[4] == 'под' else measurement[4],
+                'ecoli': measurement[len(measurement) - 1]
+            }
+            tableResults.append(result)
+        elif lineItems[i].startswith('Географски координати:'):
+            coordinates = lineItems[i].split(' ')
+            coord_x = coordinates[len(coordinates) - 1].strip('.')
+            i += 2
+            coordinates = lineItems[i].split(' ')
+            coord_x += ' ' + coordinates[0]
+            coord_y = coordinates[len(coordinates) - 1].strip('.')
+            i += 3
+            coord_y += ' ' + lineItems[i]
+        elif lineItems[i].startswith('Пункт за вземане на проби'):
+            pointIndex = lineItems[i].index('№')
+            point = lineItems[i][pointIndex + 1: lineItems[i].index(' ', pointIndex + 2)].strip(' ').lstrip('.')
+            nameIndex = lineItems[i].index('Наименование')
+            name = lineItems[i][nameIndex + 12 : len(lineItems[i])].strip()
+        elif lineItems[i].startswith('стр.'):
+            for tr in tableResults:
+                coordinates = degreesToDecimal(coord_x, coord_y);
+                tr['coord_x'] = coordinates[0]
+                tr['coord_y'] = coordinates[1]
+                tr['point'] = point
+                tr['name'] = name
+                results.append(tr)
 
+            tableResults = []
+            point = ''
+            name = ''
 
-def extractXlsFile(file_name):
-    df = pd.read_excel(file_name, header=None)
-    return df.values
-
-
-def procesMeasIndex(value):
-    if isinstance(value, int):
-        return value
-    elif value.isnumeric():
-        return int(value)
-    elif value.startswith("под"):
-        return 10
-    else:
-        return 0
-
-def processMeasurement(row):
-    if is_nan(row[2]) & is_nan(row[3]):
-        return None
-
-    date = row[1]
-
-    if date != '':
-        penter = row[2]
-        esher = row[3]
-        regex = r"\s*г."
-
-        parsedDate = None
-        if type(date) is datetime:
-            parsedDate = date
-        else:
-            parsedDate = datetime.strptime(re.sub(regex,"", date), "%d.%m.%Y")
-        parsedPenter = type
-
-        parsedPenter = procesMeasIndex(penter)
-        parsedEsher = procesMeasIndex(esher)
-
-        measurement = {'date': parsedDate.isoformat(),
-                       'penter': parsedPenter,
-                       'ecoli': parsedEsher
-                       }
-
-        return measurement
-    return None
-
-
-def processTable(dataArray):
-    res = []
-    for i in range(1, len(dataArray)):
-        measurement = processMeasurement(dataArray[i])
-        if measurement is not None:
-            res.append(measurement)
-        else:
-            break
-    return res
-
-
-def extractPdfText(file_name):
-    pdfReader = PyPDF2.PdfFileReader(file_name)
-    count = pdfReader.numPages
-
-    for i in range(count):
-        page = pdfReader.getPage(i)
-        print(page.extractText())
-
-
-def processBeach(dataArray, rowIndex):
-    id = dataArray[rowIndex][2]
-    title = dataArray[rowIndex][5]
-    coordXStr = dataArray[rowIndex + 1][3]
-    coordYStr = dataArray[rowIndex + 1][5]
-
-    coordinates = degreesToDecimal(coordXStr, coordYStr);
-
-    coordX = coordinates[0]
-    coordY = coordinates[1]
-
-    i = rowIndex
-    print(len(dataArray))
-    while i < len(dataArray) and dataArray[i][0] != 'Планирана дата за пробонабиране, съгласно графика за мониторинг' : i += 1
-
-    mArr = []
-    i+=1
-    m = processMeasurement(dataArray[i])
-    while m is not None:
-        mArr.append(m)
-        i+=1
-        m = processMeasurement(dataArray[i])
-
-
-    result = []
-    for measurement in mArr:
-        result.append({
-            'point': id,
-            'title': title,
-            'coord_x': coordX,
-            'coord_y': coordY,
-            'measurement_date': measurement['date'],
-            'intestinal_enterococci': measurement['penter'],
-            'ecoli': measurement['ecoli']
-        })
-    return result
-
-def processBurgas(dataArray):
-    i = 0
-    beaches = []
-    while i<len(dataArray):
-        if dataArray[i][0] == 'ПУНКТ ЗА ВЗЕМАНЕ НА ПРОБИ №':
-            b = processBeach(dataArray,i)
-            beaches += b
-        i += 1
-    return beaches
+    return results
 
 download(VARNA_URL, VARNA_FILE_NAME)
-download(BURGAS_URL, BURGAS_FILE_NAME)
-
-# rawDataVarna = extractPdfFile(VARNA_FILE_NAME, "1-2") # "1-end"
-
-# for d in rawDataVarna:
-#     table = processTable(d)
-
-rawDataBurgas = extractXlsFile(BURGAS_FILE_NAME)
-burgasBeaches = processBurgas(rawDataBurgas)
-
-# INSERTTT----------------------------
-
-secret_name = "MONGO_URI"
-region_name = "eu-west-1"
-
-# Create a Secrets Manager client
-session = boto3.session.Session()
-client = session.client(
-    service_name='secretsmanager',
-    region_name=region_name
-)
-
-get_secret_value_response = client.get_secret_value(
-    SecretId=secret_name
-)
-
-secret_value = json.loads(get_secret_value_response['SecretString'])
-
-username = urllib.parse.quote_plus(secret_value['username'])
-password = urllib.parse.quote_plus(secret_value['password'])
-host = secret_value['host']
-
-print(host)
-
-mongo_client = MongoClient('mongodb://%s:%s@%s:27017' % (username, password, host))
-
-db = mongo_client.seals
-collection = db.get_collection('beaches')
-books_to_update = collection.insert_many(burgasBeaches)
+beaches_varna = processFile(VARNA_FILE_NAME)
+y = json.dumps(beaches_varna)
+print(y)
